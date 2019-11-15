48ace3f57e0b0138ad811d3237acab93e153ad3e
Rahul Pol <rahul.h.pol@gmail.com>
2019-11-15 15:04:21 +0530

    Updated Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ElectronService } from "../core/services";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  color = "warn";
+  mode = "indeterminate";
 
-  constructor() { }
+  selectedFile: any = null;
+  fileresult: any;
+  isFileSelected: boolean;
 
-  ngOnInit() {
+  constructor(private electron: ElectronService) {}
+
+  ngOnInit() {}
+
+  ExitWindow() {
+    this.electron.window.close();
   }
 
+  MinWindow() {
+    this.electron.window.minimize();
+  }
+
+  FileOpenDialog() {
+    const { dialog } = require("electron").remote;
+
+    this.fileresult = dialog.showOpenDialogSync(this.electron.window, {
+      title: "Select the Excel File to be Processed",
+      defaultPath: "C:\\",
+      filters: [{ name: "Excel Files", extensions: ["xls", "xlsx"] }],
+      properties: ["openFile", "multiSelections"]
+    });
+    if (this.fileresult === undefined) {
+      this.selectedFile = null;
+      this.isFileSelected = false;
+    } else {
+      this.selectedFile = this.fileresult;
+      this.isFileSelected = true;
+    }
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeRoutingModule } from './home-routing.module';
+import { HomeRoutingModule } from "./home-routing.module";
 
-import { HomeComponent } from './home.component';
-import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from "./home.component";
+import { SharedModule } from "../shared/shared.module";
+
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatProgressBarModule
+} from "@angular/material";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [
+    CommonModule,
+    SharedModule,
+    HomeRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatProgressBarModule
+  ]
 })
 export class HomeModule {}
