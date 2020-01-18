4e85bb241cd0873677fa211f6a56ceacea05cb90
rahulhiralalpol <rahul.h.pol@gmail.com>
2020-01-18 08:03:55 +0530

    Added settings

--- src/app/home/fileselect/fileselect.component.ts
+++ src/app/home/fileselect/fileselect.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { ElectronService } from "../../core/services";
 import { GeneralService } from "../../core/services/general/general.service";
+import * as ElectronPrefs from "electron-prefs";
+import * as path from "path";
 
 @Component({
   selector: "app-fileselect",
@@ -9,8 +11,11 @@ import { GeneralService } from "../../core/services/general/general.service";
 })
 export class FileselectComponent implements OnInit {
   value;
+  prefs;
+  defaultPath;
+  filePath;
 
-  selectedFile: any = null;
+  selectedFile: string = null;
   fileresult: any;
   isFileSelected: boolean;
 
@@ -25,14 +30,23 @@ export class FileselectComponent implements OnInit {
 
   ngOnInit() {
     this.generalservice.ResizeToOriginal();
+    this.prefs = new ElectronPrefs({
+      fileName: "config.js"
+    });
   }
 
   FileOpenDialog() {
     const { dialog } = require("electron").remote;
 
+    if (this.prefs.get("lastFileOpenedDirectory")) {
+      this.defaultPath = this.prefs.get("lastFileOpenedDirectory");
+    } else {
+      this.defaultPath = "C:\\";
+    }
+
     this.fileresult = dialog.showOpenDialogSync(this.electron.window, {
       title: "Select the Excel File to be Processed",
-      defaultPath: "C:\\",
+      defaultPath: this.defaultPath,
       filters: [{ name: "Excel Files", extensions: ["xls", "xlsx"] }],
       properties: ["openFile", "multiSelections"]
     });
@@ -42,6 +56,10 @@ export class FileselectComponent implements OnInit {
     } else {
       this.selectedFile = this.fileresult;
       this.isFileSelected = true;
+      alert(this.selectedFile);
+      alert(path.dirname("C:/Windows/Fonts/8514fix.fon"));
+      alert(path.dirname(this.selectedFile.replace(/\\/g, "/")));
+      this.prefs.set("lastFileOpenedDirectory", "C:\\");
     }
   }
 
--- src/app/home/settings/settings.component.ts
+++ src/app/home/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { GeneralService } from "../../core/services";
+import * as ElectronPrefs from "electron-prefs";
 
 @Component({
   selector: "app-settings",
@@ -8,10 +9,15 @@ import { GeneralService } from "../../core/services";
   styleUrls: ["./settings.component.scss"]
 })
 export class SettingsComponent implements OnInit {
+  prefs;
+
   constructor(private router: Router, private generalservice: GeneralService) {}
 
   ngOnInit() {
     this.generalservice.ResizeToOriginal();
+    this.prefs = new ElectronPrefs({
+      fileName: "config.js"
+    });
   }
 
   backtohome() {
@@ -19,6 +25,7 @@ export class SettingsComponent implements OnInit {
   }
 
   saveandbacktohome() {
+    alert(this.prefs.get("lastFileOpenedDirectory"));
     this.router.navigate(["fileselect"]);
   }
 }
