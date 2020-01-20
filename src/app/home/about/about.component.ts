93df7b61cfefbacc1c337bc0fd836ae83c936432
Rahul Pol <rahul.h.pol@gmail.com>
2020-01-20 17:27:58 +0530

    Cosmetic Changes in About Tab

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { GeneralService } from "../../core/services";
+import { MatIconRegistry } from "@angular/material";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: "app-about",
@@ -8,7 +10,31 @@ import { GeneralService } from "../../core/services";
   styleUrls: ["./about.component.scss"]
 })
 export class AboutComponent implements OnInit {
-  constructor(private router: Router, private generalservice: GeneralService) {}
+  constructor(
+    private router: Router,
+    private generalservice: GeneralService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      "facebook",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        "assets/icons/facebook.svg"
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      "twitter",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        "assets/icons/twitter.svg"
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      "linkedin",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        "assets/icons/linkedin.svg"
+      )
+    );
+  }
 
   ngOnInit() {
     this.generalservice.ResizeToOriginal();
